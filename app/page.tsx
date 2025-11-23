19fb8eb35253a3d36aca76f35911d10fc0f7fccd
ZohaibArshad45 <Zohaibfta@gmail.com>
2025-11-23 20:08:03 +0500

    Home link add on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <Link href="/home" className="text-blue-600 hover:underline">
+        Home
+      </Link>
     </div>
   );
 }
